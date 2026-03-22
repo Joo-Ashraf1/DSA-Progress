894423215fd225feebc9f9f5f9cfd8c266dac277
Youssef Ashraf <youseefashraf54@gmail.com>
2026-03-23 01:11:56 +0200

    Successor in BST

--- BST.cpp
+++ BST.cpp
@@ -62,5 +62,58 @@ public:
             }
         }
     }
+    int minValue(){
+        BST* node=this;
+        while(node&& node->left){
+            node=node->left;
+        }
+        return node->data;
+    }
+
+    bool findChain(vector<BST*> &ancestors,int target){
+        ancestors.push_back(this);
+        if(data==target){
+            return true;
+        }
+        else if(data<target){
+            return left&&left->findChain(ancestors,target);
+        }
+        else{
+            return right&& right->findChain(ancestors,target);
+        }
+    }
+
+    BST* getNextParent(vector<BST*> &ancestors){
+        if(ancestors.size()==0){
+            return nullptr;
+        }
+        BST* node=ancestors.back();
+        ancestors.pop_back();
+        return node;
+
+    } 
+
+    pair<int,bool> Successor(int target){
+        vector<BST*> ancestors;
+        if (!findChain(ancestors, target))
+			return make_pair(false, -1);
+
+		BST* child = getNextParent(ancestors);
+
+		if (child->right)	// must have be in min of right
+			return make_pair(true, child->right->minValue());
+
+		BST* parent = getNextParent(ancestors);
+
+		// Cancel chain of ancestors I am BIGGER than them
+		while (parent && parent->right == child)
+			child = parent, parent = getNextParent(ancestors);
+
+		if (parent)	//
+			return make_pair(true, parent->data);
+		return make_pair(false, -1);
+
+
+    }
     
 };
